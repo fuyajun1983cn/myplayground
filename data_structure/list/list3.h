645878dea127a1435fd4929f3141e69ec53c25f1
Yajun Fu <fuyajun1983cn@163.com>
2015-04-25 23:39:53 +0800

    链式线程表的基本操作实现 1. 确定链表的长度 2. 查找第K个元素 3. 在链表中搜索元素 4. 输出链表 5. 从链表中删除元素 6. 在链表中插入元素

---
+++ data_structure/list/list3.h
@@ -0,0 +1,188 @@
+#ifndef _LIST3_H
+#define _LIST3_H
+/**
+ * 线性表的链式存储实现
+ *
+ */
+#include <iostream>
+#include "xcept.h"
+
+using namespace std;
+
+template<typename T>
+class Chain;
+
+template<typename T>
+class ChainNode
+{
+    friend Chain<T>;
+
+private:
+    T data;
+    ChainNode<T> *link;
+};
+
+template<typename T>
+class Chain
+{
+public:
+    Chain() { first = 0; }
+    ~Chain();
+    bool IsEmpty() const { return first == 0; }
+    int Length() const;
+    bool Find(int k, T& x) const;
+    int Search(const T& x) const;
+    Chain<T>& Delete(int k, T& x);
+    Chain<T>& Insert(int k, const T& x);
+    void Output(ostream& out) const;
+
+private:
+    ChainNode<T> *first; //指向第一个结点的指针
+};
+
+/**
+ * 析构函数，删掉链表中的所有函数
+ */
+template<typename T>
+Chain<T>::~Chain()
+{
+    ChainNode<T> *next;
+    while (first) {
+	next = first->link;
+	delete first;
+	first = next;
+    }
+}
+
+/**
+ * 返回链表中的元素总数
+ */
+template<typename T>
+int Chain<T>::Length() const
+{
+    ChainNode<T> *node = first;
+    int length = 0;
+
+    while(node) {
+	node = node->link;
+	length++;
+    }
+
+    return length;
+}
+
+template<typename T>
+bool Chain<T>::Find(int k, T& x) const
+{
+    if (k < 1) return false;
+    ChainNode<T> *current = first;
+
+    int index = 1;
+
+    while (index < k && current) {
+	current = current->link;
+	index++;
+    }
+
+    if (current) {
+	x = current->data;
+	return true;
+    }
+
+    return false;
+}
+
+template<typename T>
+int Chain<T>::Search(const T& x) const
+{
+    ChainNode<T> *current = first;
+    int index = 1;
+
+    while (current && current->data != x) {
+	index++;
+	current = current->link;
+    }
+
+    if (current)
+	return index;
+    return 0;
+} 
+
+template<typename T>
+void Chain<T>::Output(ostream& out) const
+{
+    ChainNode<T> *current = first;
+    while (current) {
+	out<<current->data<<" ";
+	current = current->link;
+    }
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const Chain<T>& x)
+{
+    x.Output(out);
+    return out;
+}
+
+template<typename T>
+Chain<T>& Chain<T>::Delete(int k, T& x)
+{
+    if (k < 1 || !first)
+	throw OutOfBounds();
+
+    ChainNode<T> *p = first;
+    ChainNode<T> *q = p->link;
+    if (k == 1) {
+	first = q;
+    } else {
+	int index = 1;
+	q = first;
+	while (index < k && q) {
+	    q = q->link;
+	    index++;
+	}
+
+	if (q && q->link) {
+	    p = q->link;
+	    q->link = p->link;
+	} else throw OutOfBounds();
+
+    }
+    x = p->data;
+    delete p;
+
+    return *this;
+
+}
+/**
+ * 在第k个元素之后插入元素
+ */
+template<typename T>
+Chain<T>& Chain<T>::Insert(int k, const T& x)
+{
+    if (k < 0) throw OutOfBounds();
+
+    ChainNode<T> *p = first;
+    int index = 1;
+    while (index < k && p) {
+	p = p->link;
+	index++;
+    }
+
+    if (k > 0 && !p) throw OutOfBounds();
+    ChainNode<T> *y = new ChainNode<T>();
+    y->data = x;
+    if (k) {
+	y->link = p->link;
+	p->link = y;
+    } else {
+	y->link = first;
+	first = y;
+    }
+
+    return *this;
+}
+
+
+#endif
--- data_structure/list/list_test.cpp
+++ data_structure/list/list_test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "list1.h"
+#include "list3.h"
 #include "xcept.h"
 using namespace std;
 
@@ -105,4 +106,96 @@ void test1()
  */
 void test2()
 {
+}
+
+void test3()
+{
+    try {
+#if 1
+	Chain<int> L;
+	cout<<"Length = "<<L.Length()<<endl;
+	cout<<"IsEmpty = "<<L.IsEmpty()<<endl;
+	L.Insert(0, 2).Insert(1,6).Insert(2, 7).Insert(3, 8).Insert(4,9);
+	cout<<"List is "<<L<<endl;
+	cout<<"IsEmpty = "<<L.IsEmpty()<<endl;
+	int z;
+        L.Find(1, z);
+	cout<<"First Element is "<<z<<endl;
+	cout<<"Length = "<<L.Length()<<endl;
+	L.Delete(1, z);
+	cout<<"Deleted element is "<<z<<endl;
+	cout<<"List is "<<L<<endl;
+	L.Delete(1,z).Delete(1,z).Delete(1,z);
+	cout<<"List is "<<L<<endl;
+	cout<<"Length = "<<L.Length()<<endl;
+
+#endif
+	//
+#if 0
+	cout<<"move around list"<<endl;
+	LinearList<int> L2;
+	L2.Insert(0,2).Insert(0,3).Insert(0,4);
+        cout<<"List is "<<L2<<endl;
+	L2.Current(z);
+	cout<<"current is "<<z<<endl;
+	L2.Next();
+	L2.Current(z);
+	cout<<"current is "<<z<<endl;
+	L2.Next();
+	L2.Current(z);
+	cout<<"current is "<<z<<endl;
+
+	L2.Previous();
+	L2.Current(z);
+	cout<<"current is "<<z<<endl;
+
+	L2.Previous();
+	L2.Current(z);
+	cout<<"current is "<<z<<endl;
+
+	L2.Previous();
+	L2.Current(z);
+	cout<<"current is "<<z<<endl;
+
+	L2.Previous();
+	L2.Current(z);
+	cout<<"current is "<<z<<endl;
+#endif
+#if 0
+	LinearList<int> L3;
+	L3.Insert(0, 1).Insert(1, 4).Insert(2, 6);
+	LinearList<int> L4;
+	L4.Insert(0, 2).Insert(1, 3).Insert(2,5);
+
+	LinearList<int> L5;
+	L5.Alternate(L3, L4);
+	cout<<"Alernate L3 & L4 is "<<L5<<endl;
+
+	LinearList<int> L6;
+	L6.Merge(L3, L4);
+	cout<<"Merge L3 & L4 is "<<L6<<endl;
+	
+	LinearList<int> L7, L8;
+	L6.Split(L7, L8);
+	cout<<"Split of L6 "<<endl;
+	cout<<"L7: "<<L7<<endl;
+	cout<<"L8: "<<L8<<endl;
+	
+#endif
+    } catch (...) {
+	cerr<<"An exception has occured"<<endl;
+    }
+}
+
+int list_test(int argc, char *argv[])
+{
+    //测试1
+    test3();
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    return list_test(argc, argv);
 }
