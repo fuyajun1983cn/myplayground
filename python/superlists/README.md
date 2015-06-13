bbc3571261ef77732835d46c3c2db7e12a6dc0fe
Yajun Fu <fuyajun1983cn@163.com>
2015-06-13 17:31:20 +0800

    First commit: First FT and basic Django config

---
+++ python/superlists/README.md
@@ -0,0 +1,3 @@
+学习ebook： Test-Driven Web Development with Python  
+学习进度：
+http://chimera.labs.oreilly.com/books/1234000000754/ch02.html
---
+++ python/superlists/functional_tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
+
