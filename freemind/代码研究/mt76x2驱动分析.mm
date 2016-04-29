963206b9fdd0bfd7db82643329cda8acd55d2257
Yajun Fu <fuyajun1983cn@163.com>
2016-04-29 20:12:13 +0800

    2016年04月29日20时12分10秒

---
+++ freemind/代码研究/mt76x2驱动分析.mm
@@ -0,0 +1,71 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#006633" CREATED="1461639260592" ID="ID_1645156134" MODIFIED="1461640982547" TEXT="mt76x2&#x9a71;&#x52a8;&#x5206;&#x6790;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#669900" CREATED="1461640571774" ID="ID_669723955" MODIFIED="1461641000163" POSITION="right" TEXT="common&#x67b6;&#x6784;"/>
+<node COLOR="#669900" CREATED="1461640585282" ID="ID_611641321" MODIFIED="1461753002388" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21151;&#33021;&#27169;&#22359;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1461640598512" ID="ID_563883277" MODIFIED="1461640616230">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Auto Channel Selection
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1461640772905" ID="ID_964227616" MODIFIED="1461640775384" TEXT="CCA">
+<node COLOR="#999999" CREATED="1461640802560" ID="ID_669922555" MODIFIED="1461640970570">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      calaulates the dirtyness of all channels by the
+    </p>
+    <p>
+      CCA value&#160;&#160;and Rssi.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node CREATED="1461640776130" ID="ID_364129693" MODIFIED="1461640799203" TEXT="Ap Number">
+<node COLOR="#999999" CREATED="1461640819956" ID="ID_1375090733" MODIFIED="1461640971561">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      calaulates the dirtyness of all channels by the dirtiness value and
+    </p>
+    <p>
+      number of AP in each channel
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
--- freemind/方法论/Breakthrough Rapid Reading.mm
+++ freemind/方法论/Breakthrough Rapid Reading.mm
@@ -3,7 +3,213 @@
 <node CREATED="1461119493594" ID="ID_1626607992" MODIFIED="1461119515005" TEXT="Breakthrough Rapid Reading">
 <node CREATED="1461203718022" ID="ID_424456436" MODIFIED="1461203727485" POSITION="right" TEXT="&#x6452;&#x5f03;&#x7684;&#x4e60;&#x60ef;">
 <node CREATED="1461203730562" ID="ID_1590549347" MODIFIED="1461203751584" TEXT="&#x9605;&#x8bfb;&#x65f6;&#xff0c;&#x5fc3;&#x91cc;&#x5728;&#x9ed8;&#x8ba4;&#x5355;&#x8bcd;"/>
+<node CREATED="1461633695918" ID="ID_167051529" MODIFIED="1461633722712">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      regression(&#36820;&#22238;&#21435;&#30475;&#20043;&#21069;&#30475;&#36807;&#30340;&#20869;&#23481;)
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1461633737314" ID="ID_301853678" MODIFIED="1461633744061">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#26377;&#24847;&#35782;&#30340;
+    </p>
+  </body>
+</html></richcontent>
 </node>
-<node CREATED="1461205996072" ID="ID_39694136" MODIFIED="1461206005536" POSITION="right" TEXT="&#x7b2c;&#x4e09;&#x7ae0;  &#x5f00;&#x59cb;&#x8bad;&#x7ec3;"/>
+<node CREATED="1461633744612" ID="ID_23107732" MODIFIED="1461633750604">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#26080;&#24847;&#35782;&#30340;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1461633792464" ID="ID_340533353" MODIFIED="1461633872936">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff6666">&#23398;&#20250;&#28040;&#38500;Regression&#65288;&#22238;&#35835;&#65289;&#26159;&#25552;&#39640;&#38405;&#35835;&#36895;&#24230;&#30340;&#31532;&#19968;&#27493;</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1461205996072" ID="ID_39694136" MODIFIED="1461206005536" POSITION="right" TEXT="&#x7b2c;&#x4e09;&#x7ae0;  &#x5f00;&#x59cb;&#x8bad;&#x7ec3;">
+<node CREATED="1461634183644" ID="ID_1661279142" MODIFIED="1461634214985" TEXT="&#x4f7f;&#x7528;&#x624b;&#x6307;&#x5e2e;&#x52a9;&#x6d88;&#x9664;Regression&#x7684;&#x4e60;&#x60ef;"/>
+</node>
+<node CREATED="1461634835386" ID="ID_601237718" MODIFIED="1461634865045" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#31532;&#22235;&#31456;&#160;&#160;&#28040;&#38500;&#22238;&#30475;&#65292;&#21152;&#36895;&#21521;&#21069;&#35835;
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1461807254017" ID="ID_1576128336" MODIFIED="1461807269485">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      how people read fast
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1461809293383" ID="ID_1490827466" MODIFIED="1461893460616">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff3333">&#32451;&#20064;1</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1461809301571" ID="ID_144501251" MODIFIED="1461893774706" TEXT="1. &#x9009;&#x62e9;&#x4e00;&#x4efd;&#x9605;&#x8bfb;&#x6750;&#x6599;&#x8bfb;3&#x904d;, &#x65f6;&#x95f4;&#x8bbe;&#x5b9a;&#x4e3a;3&#x5206;&#x949f;"/>
+<node CREATED="1461809330770" ID="ID_109494769" MODIFIED="1461809342714" TEXT="2. &#x4ee5;&#x8f83;&#x5feb;&#x7684;&#x901f;&#x5ea6;&#x518d;&#x8bfb;&#x4e00;&#x904d;"/>
+<node CREATED="1461809343318" ID="ID_756482963" MODIFIED="1461809356735" TEXT="3. &#x4ee5;&#x66f4;&#x5feb;&#x7684;&#x901f;&#x5ea6;&#x518d;&#x8bfb;&#x4e00;&#x904d;"/>
+<node CREATED="1461809357225" ID="ID_854771483" MODIFIED="1461809375436" TEXT="4. &#x4ee5;&#x6700;&#x5feb;&#x7684;&#x901f;&#x5ea6;&#x518d;&#x8bfb;&#x4e00;&#x904d;"/>
+<node CREATED="1461809376494" ID="ID_178241671" MODIFIED="1461809421938" TEXT="5. &#x91cd;&#x65b0;&#x627e;&#x4e00;&#x6bb5;&#x672a;&#x8bfb;&#x8fc7;&#x7684;&#x8d44;&#x6599;&#xff0c;&#x4ee5;&#x6700;&#x5feb;&#x7684;&#x901f;&#x5ea6;&#x8bfb;&#x5b8c;&#xff0c;&#x8bb0;&#x4e0b;&#x65f6;&#x95f4;&#xff0c;&#x7edf;&#x8ba1;&#x9605;&#x8bfb;&#x901f;&#x7387;"/>
+</node>
+<node CREATED="1461893462732" ID="ID_1845708466" MODIFIED="1461893468549">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#32451;&#20064;2
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1461893473312" ID="ID_124708732" MODIFIED="1461893563231">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1. &#36873;&#25321;&#19968;&#20221;&#38405;&#35835;&#26448;&#26009;&#65292;&#35835;&#19977;&#20998;&#38047;&#65292;&#26102;&#38388;&#21040;&#30340;&#26102;&#20505;&#65292;&#35760;&#19979;&#26631;&#35782;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1461893565062" ID="ID_1961248085" MODIFIED="1461893604874">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2. &#37325;&#26032;&#35835;3&#20998;&#38047;&#65292;&#22914;&#26524;&#21040;&#36798;&#26631;&#35760;&#22788;&#65292;&#26102;&#38388;&#36824;&#27809;&#26377;&#21040;&#65292;&#21017;&#36820;&#22238;&#24320;&#22836;&#32487;&#32493;&#21335;&#35835;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1461893606144" ID="ID_1397533302" MODIFIED="1461893618748">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3. &#20351;&#29992;2&#20998;&#38047;&#37325;&#26032;&#35835;&#19968;&#36941;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1461893619733" ID="ID_1576049474" MODIFIED="1461893677640">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      4. &#20351;&#29992;1&#20998;&#38047;&#37325;&#26032;&#35835;&#19968;&#36941;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1461893678640" ID="ID_1173509203" MODIFIED="1461893706755">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#32451;&#20064;&#23436;&#25104;&#21518;&#65292;&#20174;&#26631;&#35760;&#22788;&#32487;&#32493;&#24320;&#22987;&#35835;&#19968;&#20998;&#38047;&#65292;&#35760;&#24405;&#19979;&#38405;&#35835;&#30340;&#36895;&#24230;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1461893210773" ID="ID_1865424587" MODIFIED="1461893212709" TEXT="tips">
+<node CREATED="1461893213679" ID="ID_1317441818" MODIFIED="1461893248281">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#ff3333">The secret to good drilling is to go faster than you can say all of the words to yourself; yet you must still be seeing all of the words.</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1461895557732" ID="ID_195211154" MODIFIED="1461895595594">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#3333ff">quickly identify what a passage is about, usually the first sentence.</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1461809809942" ID="ID_1284567044" MODIFIED="1461809811993" POSITION="right" TEXT="ASK THIS SIMPLE"/>
+<node CREATED="1461634455525" ID="ID_1212230188" MODIFIED="1461634461994" POSITION="left" TEXT="&#x9605;&#x8bfb;&#x901f;&#x5ea6;&#x8bb0;&#x5f55;">
+<node CREATED="1461809046564" ID="ID_1713464106" MODIFIED="1461809069121" TEXT="&#x5f53;&#x524d;&#x9605;&#x8bfb;&#x901f;&#x5ea6;213&#x5b57;&#x6bcf;&#x5206;&#x949f;"/>
+<node CREATED="1461894944420" ID="ID_539874512" MODIFIED="1461894952079" TEXT="276&#x5b57;&#x6bcf;&#x5206;&#x949f;"/>
+</node>
 </node>
 </map>
