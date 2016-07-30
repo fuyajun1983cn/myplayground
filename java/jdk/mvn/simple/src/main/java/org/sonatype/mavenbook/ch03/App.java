c7c230ce4fb3938c13bd3f7763c6770b3f04ceee
Yajun Fu <fuyajun1983cn@163.com>
2016-07-30 13:41:15 +0800

    2016年07月30日13时41分15秒

---
+++ java/jdk/mvn/simple/src/main/java/org/sonatype/mavenbook/ch03/App.java
@@ -0,0 +1,13 @@
+package org.sonatype.mavenbook.ch03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
