c8d49de0537b50574699ff948ee302506c44786e
Yajun Fu <fuyajun1983cn@163.com>
2016-09-24 11:53:38 +0800

    2016年09月24日11时53分38秒

--- diy/miniui/device.cpp
+++ diy/miniui/device.cpp
@@ -5,18 +5,42 @@
 #include <QEvent>
 #include <QKeyEvent>
 #include <QPainter>
-#include <QPushButton>
-#include <QLayout>
 #include <QDebug>
 
-//http://blog.csdn.net/vince025/article/details/17552497
+//Test
+#include <QPushButton>
+#include <QVBoxLayout>
 
-#ifdef Q_OS_LINUX
-#include <X11/extensions/shape.h>
-#endif
-#ifdef Q_OS_WIN
-#include <windows.h>
-#endif
+#include <QStaticText>
+struct DrawingItem
+{
+    enum ItemType {
+        RECT,
+        TEXT,
+        IMG,
+        ItemType_NUM,
+    } eType;
+
+    union {
+        struct {
+            int x, y;
+            int width, height;
+            struct {
+                int r,g,b,a;
+            } backgroundColor;
+        }eRect;
+
+        struct {
+            const char *text;
+            int x, y;
+        }eText;
+
+        struct {
+            const char *path;
+            int x,y;
+        }eImage;
+    };
+};
 
 static QWidget *pMainScreen = 0;
 
@@ -24,33 +48,19 @@ Device::Device()
 {
     pMainScreen = new QWidget(0, Qt::FramelessWindowHint
                               | Qt::WindowSystemMenuHint);
-    pMainScreen->setAttribute(Qt::WA_InputMethodEnabled);
-    QPushButton *btn = new QPushButton(pMainScreen);
-    btn->setText("This is a Button");
-    QVBoxLayout *layout = new QVBoxLayout();
-    layout->addWidget(btn);
-    pMainScreen->setLayout(layout);
-#if 0
-    QPalette pal = pMainScreen->palette();
-    pal.setColor(QPalette::Background, QColor(100,100,100,0));
-    pMainScreen->setPalette(pal);
-    pMainScreen->setAutoFillBackground(true);
-#endif
     pMainScreen->setAttribute(Qt::WA_TranslucentBackground, true);
-#ifdef Q_OS_LINUX
-    XShapeCombineRectangles(QX11Info::display(), winId(), ShapeInput, 0,
-                                0, NULL, 0, ShapeSet, YXBanded);
-#endif
-#ifdef Q_OS_WIN
-        SetWindowLong(winId(), GWL_EXSTYLE, GetWindowLong(winId(), GWL_EXSTYLE) |
-                      WS_EX_TRANSPARENT | WS_EX_LAYERED);
-#endif
-//    pMainScreen->setAttribute(Qt::WA_TransparentForMouseEvents,true);
+
+
+    QPushButton *btn = new QPushButton(0);
+    btn->setText("Test");
+    QVBoxLayout *layout = new QVBoxLayout(pMainScreen);
+    layout->addWidget(btn);
+    connect(btn, SIGNAL(clicked()), this, SLOT(test()));
+
+
     QFont ft = pMainScreen->font();
     ft.setPointSize(20);
     pMainScreen->setFont(ft);
-//    pMainScreen->resize(300, 460);
- //   pMainScreen->show();
     pMainScreen->showFullScreen();
 }
 
@@ -69,11 +79,13 @@ bool Device::eventFilter(QObject *object, QEvent *event)
 {
     if (event->type() == QEvent::Paint && object == pMainScreen) {
 
-        qDebug()<<"Screen Width: "<<pMainScreen->geometry().width();
-        qDebug()<<"Screen Height: "<<pMainScreen->geometry().height();
+ //       qDebug()<<"Screen Width: "<<pMainScreen->geometry().width();
+ //       qDebug()<<"Screen Height: "<<pMainScreen->geometry().height();
 
         QPainter p(pMainScreen);
         p.fillRect(pMainScreen->rect(), QColor(0,0xff,0,60));
+
+        drawStuff(&p);
 
         return true;
     } else if (event->type() == QEvent::KeyRelease) {
@@ -84,4 +96,66 @@ bool Device::eventFilter(QObject *object, QEvent *event)
         }
     }
     return false;
+}
+
+void Device::test()
+{
+#if 1
+    {
+    DrawingItem *rect = new DrawingItem();
+    rect->eType = DrawingItem::RECT;
+    rect->eRect = {300, 300, 300, 400, {0,0,0xff,0xff}};
+    addDrawingItem(rect);
+    }
+#endif
+
+#if 1
+    {
+    DrawingItem *rect = new DrawingItem();
+    rect->eType = DrawingItem::RECT;
+    rect->eRect = {100, 100, 300, 400, {0xff, 0, 0, 0xff}};
+    addDrawingItem(rect);
+    }
+#endif
+}
+
+void Device::addDrawingItem(struct DrawingItem*item)
+{
+    if (item != NULL) {
+        items.push_back(item);
+        qDebug("Adding Drawing Item");
+        pMainScreen->update();
+    }
+}
+
+void Device::drawStuff(QPainter *p)
+{
+    qDebug()<<"Drawing Stuffs";
+    while (!items.empty()) {
+        DrawingItem *item = items.front();
+        switch (item->eType) {
+        case DrawingItem::RECT:
+            qDebug("Drawing Rect");
+            p->fillRect(item->eRect.x, item->eRect.y,
+                                        item->eRect.width, item->eRect.height,
+                                        QColor(item->eRect.backgroundColor.r,
+                                               item->eRect.backgroundColor.g,
+                                               item->eRect.backgroundColor.b,
+                                               item->eRect.backgroundColor.a));
+            break;t
+        case DrawingItem::TEXT:
+            p->drawStaticText(item->eText.x,
+                                                item->eText.y,
+                                                QStaticText(item->eText.text));
+            break;
+        case DrawingItem::IMG:
+
+            break;
+        default:
+            break;
+        }
+         items.pop_front();
+        free(item);
+        item = NULL;
+    }
 }
--- diy/miniui/device.h
+++ diy/miniui/device.h
@@ -2,19 +2,31 @@
 #define DEVICE_H
 
 #include <QObject>
+#include <QList>
+
+class QPainter;
+struct DrawingItem;
 
 class Device : public QObject
 {
     Q_OBJECT
 public:
     static Device *getInstance();
+    void addDrawingItem(struct DrawingItem*);
 
 protected:
     virtual bool eventFilter(QObject *, QEvent *);
 
+protected slots:
+    void test();
+
 private:
     Device();
     ~Device();
+    void drawStuff(QPainter*);
+
+private:
+    QList<DrawingItem*> items;
 };
 
 #endif // DEVICE_H
